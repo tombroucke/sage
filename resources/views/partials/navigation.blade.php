46c969450aa36090cc004507c300b258bc5e02c3
tombroucke <tom@tombroucke.be>
2022-02-21 11:17:47 +0100

    menu item label raw

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -4,14 +4,14 @@
       @foreach ($navigation as $item)
       <li class="menu-item {{ $item->classes ?? '' }} {{ $item->active ? 'menu-item--active' : '' }} {{ $item->children ? 'menu-item--has-submenu' : '' }}">
         <a href="{{ $item->url }}">
-            {{ $item->label }}
+            {!! $item->label !!}
           </a>
           @if ($item->children)
             <ul class="submenu list-unstyled">
               @foreach ($item->children as $child)
                 <li class="submenu-item {{ $child->classes ?? '' }} {{ $child->active ? 'submenu-item--active' : '' }}">
                   <a href="{{ $child->url }}">
-                    {{ $child->label }}
+                    {!! $child->label !!}
                   </a>
                 </li>
               @endforeach
