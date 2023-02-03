b9946e308c45b886c2e639f25e2c7c2ee8dcd365
tombroucke <tom@tombroucke.be>
2023-02-03 09:26:04 +0100

    Added button class

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -3,9 +3,15 @@
     <ul class="d-flex list-unstyled m-0">
       @foreach ($navigation as $item)
         <li class="menu-item {{ $item->classes ?? '' }} {{ $item->active ? 'menu-item--active' : '' }} {{ $item->children ? 'menu-item--has-submenu' : '' }}">
-          <a href="{{ $item->url }}">
+          @if(isset($item->buttonTheme))
+            <x-button :href="$item->url" :theme="$item->buttonTheme">
               {!! $item->label !!}
-          </a>
+            </x-button>
+          @else
+            <a href="{{ $item->url }}">
+                {!! $item->label !!}
+            </a>
+          @endif
           @if ($item->children)
             @include('partials.navigation-children', ['children' => $item->children])
           @endif
