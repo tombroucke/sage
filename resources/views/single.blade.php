ced086b09e62c4698d896ade49d94fc454beec6f
tombroucke <tom@tombroucke.be>
2023-10-26 17:06:49 +0200

    added sidebar

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -1,7 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-  @while(have_posts()) @php(the_post())
-    @includeFirst(['partials.content-single-' . get_post_type(), 'partials.content-single'])
-  @endwhile
+
+<div class="wp-block-columns {{ is_active_sidebar('sidebar-primary') ? 'alignwide' : '' }} is-layout-flex wp-block-columns-is-layout-flex">
+  <div class="wp-block-column is-layout-flow wp-block-column-is-layout-flow">
+    @while(have_posts()) @php(the_post())
+      @includeFirst(['partials.content-single-' . get_post_type(), 'partials.content-single'])
+    @endwhile
+  </div>
+
+  @include('sections.sidebar')
+</div>
 @endsection
