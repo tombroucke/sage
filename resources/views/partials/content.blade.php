1bf8b6ab528e28ffe4a84174147d5f2e769c1da1
tombroucke <tom@tombroucke.be>
2023-10-31 10:04:18 +0100

    Update content.blade.php

--- resources/views/partials/content.blade.php
+++ resources/views/partials/content.blade.php
@@ -1,6 +1,13 @@
 <article @php(post_class())>
   <header>
-    <h2 class="entry-title">
+    @if (get_the_post_thumbnail())
+      <div class="post__image mb-3">
+        <a href="{{ get_permalink() }}">
+          {!! get_the_post_thumbnail(null, 'thumbnail') !!}
+        </a>
+      </div>
+    @endif
+    <h2 class="post__title">
       <a href="{{ get_permalink() }}">
         {!! $title !!}
       </a>
@@ -9,7 +16,7 @@
     @include('partials.entry-meta')
   </header>
 
-  <div class="entry-summary">
+  <div class="post__summary">
     @php(the_excerpt())
   </div>
 </article>
