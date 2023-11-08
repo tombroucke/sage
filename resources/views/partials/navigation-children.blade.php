542f26892f8c808919354c853e736a4d546fc0ad
tombroucke <tom@tombroucke.be>
2023-11-09 00:32:29 +0100

    Update navigation-children.blade.php

--- resources/views/partials/navigation-children.blade.php
+++ resources/views/partials/navigation-children.blade.php
@@ -1,7 +1,7 @@
 <ul class="submenu list-unstyled">
   @foreach ($children as $child)
     <li class="submenu-item {{ $child->classes ?? '' }} {{ $child->active ? 'submenu-item--active' : '' }} {{ $child->children ? 'menu-item--has-submenu' : '' }}">
-      <a href="{{ $child->url }}">
+      <a href="{{ $child->url }}" target="{{ $child->target }}">
         {!! $child->label !!}
       </a>
       @if ($child->children)
