974dd6d3b0b227fefdb13f3e3d695dcd9f2f7073
tombroucke <tom@tombroucke.be>
2023-11-09 00:28:50 +0100

    Update navigation.blade.php

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -4,11 +4,11 @@
       @foreach ($navigation as $item)
         <li class="menu-item {{ $item->classes ?? '' }} {{ $item->active ? 'menu-item--active' : '' }} {{ $item->children ? 'menu-item--has-submenu' : '' }}">
           @if(isset($item->buttonTheme))
-            <x-button :href="$item->url" :theme="$item->buttonTheme">
+            <x-button :href="$item->url" :theme="$item->buttonTheme" :target="$item->target">
               {!! $item->label !!}
             </x-button>
           @else
-            <a href="{{ $item->url }}">
+            <a href="{{ $item->url }}" target="{{ $item->target }}">
                 {!! $item->label !!}
             </a>
           @endif
