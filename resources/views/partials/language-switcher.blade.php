5eb6105a09d002150bb48796649d302a8df77daf
tombroucke <tom@tombroucke.be>
2024-02-29 10:07:09 +0100

    Update language-switcher.blade.php

--- resources/views/partials/language-switcher.blade.php
+++ resources/views/partials/language-switcher.blade.php
@@ -1,11 +1,11 @@
 @if (!empty($inactiveLanguages))
   <div class="dropdown">
-    <button class="btn btn-secondary dropdown-toggle text-uppercase" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
       {!! $activeLanguage->native_name !!}
     </button>
     <ul class="dropdown-menu">
       @foreach ($inactiveLanguages as $inactiveLanguage)
-        <li><a class="dropdown-item text-uppercase" href="{{ $inactiveLanguage->url }}">{{ $inactiveLanguage->native_name }}</a></li>
+        <li><a class="dropdown-item" href="{{ $inactiveLanguage->url }}">{{ $inactiveLanguage->native_name }}</a></li>
       @endforeach
     </ul>
   </div>
