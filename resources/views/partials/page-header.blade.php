b006c2a25ef0773d0d75eedd1c3046f233e82a34
tombroucke <tom@tombroucke.be>
2025-06-16 17:58:11 +0200

    Update page-header.blade.php

--- resources/views/partials/page-header.blade.php
+++ resources/views/partials/page-header.blade.php
@@ -1,6 +1,6 @@
 <div class="page-header alignwide bg-light mb-5 rounded py-5 text-center">
   <div class="spacing-outer">
-    <h1>{!! esc_html($title) !!}</h1>
+    <h1>{!! wp_kses($title, $allowedInlineTags()) !!}</h1>
     {{-- @include('partials.breadcrumb') --}}
   </div>
 </div>
