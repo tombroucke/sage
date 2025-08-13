d7c908276ec348bef1429172759598d9eef91524
tombroucke <tom@tombroucke.be>
2025-08-13 09:21:54 +0200

    Update SocialMedia.php

--- app/View/Composers/SocialMedia.php
+++ app/View/Composers/SocialMedia.php
@@ -2,8 +2,7 @@
 
 namespace App\View\Composers;
 
-// TODO: replace FunctionalityPluginNamespace
-use FunctionalityPluginNamespace\Facades\SocialMedia as SocialMediaFacade;
+use BrandonBranda\Facades\SocialMedia as SocialMediaFacade;
 use Roots\Acorn\View\Composer;
 
 class SocialMedia extends Composer
@@ -19,14 +18,20 @@ class SocialMedia extends Composer
 
     public function socialMediaChannels()
     {
+        $order = ['linkedin', 'instagram', 'facebook'];
+
         return SocialMediaFacade::channels()
             ->map(function ($channel, $key) {
-                $channel['icon'] = str_replace('facebook', 'facebook-f', $channel['icon']);
+                // $channel['icon'] = str_replace('facebook', 'facebook-f', $channel['icon']);
 
                 return $channel;
             })
-            ->sortBy(function ($channel) {
-                return $channel['label'];
+            ->sortBy(function ($channel, $key) use ($order) {
+                $pos = array_search($key, $order);
+
+                return $pos === false
+                    ? count($order)
+                    : $pos;
             });
     }
 }
